3b6dc9ca480926ed74213e0cc7f2499495bb99b0
Jared Alonso <cacahuate.1895@gmail.com>
2022-10-07 20:25:13 -0500

    Mostrando productos en la interfaz grafica

--- PresentacionAgencia/FrmProductos.Designer.cs
+++ PresentacionAgencia/FrmProductos.Designer.cs
@@ -28,13 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
             this.btnrAgregar = new PresentacionAgencia.ButtonRound();
             this.btnrCerrar = new PresentacionAgencia.ButtonRound();
-            this.dgvProductos = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             this.SuspendLayout();
@@ -48,7 +49,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1201, 100);
-            this.panel1.TabIndex = 0;
+            this.panel1.TabIndex = 3;
             // 
             // label1
             // 
@@ -73,38 +74,54 @@
             this.textBox1.Location = new System.Drawing.Point(129, 126);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(910, 26);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.TabIndex = 0;
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.AllowUserToAddRows = false;
+            this.dgvProductos.AllowUserToDeleteRows = false;
+            this.dgvProductos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProductos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(42, 183);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.ReadOnly = true;
+            this.dgvProductos.RowHeadersVisible = false;
+            this.dgvProductos.Size = new System.Drawing.Size(1106, 464);
+            this.dgvProductos.TabIndex = 3;
+            this.dgvProductos.TabStop = false;
             // 
             // btnrAgregar
             // 
             this.btnrAgregar.BackgroundImage = global::PresentacionAgencia.Properties.Resources.agregarGreen_32;
+            this.btnrAgregar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnrAgregar.FlatAppearance.BorderSize = 0;
             this.btnrAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnrAgregar.Location = new System.Drawing.Point(1079, 123);
             this.btnrAgregar.Name = "btnrAgregar";
             this.btnrAgregar.Size = new System.Drawing.Size(32, 32);
-            this.btnrAgregar.TabIndex = 3;
+            this.btnrAgregar.TabIndex = 1;
             this.btnrAgregar.UseVisualStyleBackColor = true;
             // 
             // btnrCerrar
             // 
             this.btnrCerrar.BackgroundImage = global::PresentacionAgencia.Properties.Resources.closeRed_32;
+            this.btnrCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnrCerrar.FlatAppearance.BorderSize = 0;
             this.btnrCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnrCerrar.Location = new System.Drawing.Point(1137, 34);
             this.btnrCerrar.Name = "btnrCerrar";
             this.btnrCerrar.Size = new System.Drawing.Size(32, 32);
-            this.btnrCerrar.TabIndex = 1;
+            this.btnrCerrar.TabIndex = 2;
             this.btnrCerrar.UseVisualStyleBackColor = true;
-            // 
-            // dgvProductos
-            // 
-            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProductos.Location = new System.Drawing.Point(42, 183);
-            this.dgvProductos.Name = "dgvProductos";
-            this.dgvProductos.RowHeadersVisible = false;
-            this.dgvProductos.Size = new System.Drawing.Size(1106, 464);
-            this.dgvProductos.TabIndex = 4;
+            this.btnrCerrar.Click += new System.EventHandler(this.btnrCerrar_Click);
             // 
             // FrmProductos
             // 
@@ -122,6 +139,7 @@
             this.Name = "FrmProductos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmProductos_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
--- PresentacionAgencia/FrmProductos.cs
+++ PresentacionAgencia/FrmProductos.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ManejadorAgencia;
+using EntidadesAgencia;
 
 namespace PresentacionAgencia
 {
     public partial class FrmProductos : Form
     {
+        private ManejadorProducto _manejadorProducto;
         public FrmProductos()
         {
             InitializeComponent();
+            _manejadorProducto = new ManejadorProducto();
+        }
+
+        #region Componentes
+        private void Actualizar()
+        {
+            _manejadorProducto.obtenerDatos(dgvProductos);
+        }
+
+        private void Cerrar()
+        {
+            this.Close();
+        }
+        #endregion
+
+        private void FrmProductos_Load(object sender, EventArgs e)
+        {
+            Actualizar();
+        }
+
+        private void btnrCerrar_Click(object sender, EventArgs e)
+        {
+            Cerrar();
         }
     }
 }
