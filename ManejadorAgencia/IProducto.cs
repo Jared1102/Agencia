86634e204ed507c65c87f2b911bb8374938836e5
Jared Alonso <cacahuate.1895@gmail.com>
2022-10-07 20:02:05 -0500

    Agregando funciones para borrar y modificar productos en manejadores

--- ManejadorAgencia/IProducto.cs
+++ ManejadorAgencia/IProducto.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EntidadesAgencia;
 
 namespace ManejadorAgencia
 {
     internal interface IProducto
     {
-        DataGridView
+        void obtenerDatos(DataGridView tabla);
+        void agregarDatos(dynamic dato);
+        void modificarDatos(dynamic dato, dynamic aux_dato);
+
+        void borrarDatos(dynamic dato);
     }
 }
--- ManejadorAgencia/ManejadorProducto.cs
+++ ManejadorAgencia/ManejadorProducto.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using AccesoDatosAgencia;
+using Crud;
 
 namespace ManejadorAgencia
 {
-    public class Class1
+    public class ManejadorProducto : IProducto
     {
+        private AccesoDatosProducto _accesoDatosProducto = new AccesoDatosProducto();
+        private Grafico _grafico = new Grafico();
+        public void agregarDatos(dynamic dato)
+        {
+            try
+            {
+                _accesoDatosProducto.AgregarProducto(dato);
+                _grafico.Mensaje(string.Format("Se agrego con exito {0}", dato.Nombre),"Agregando Producto",MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                _grafico.Mensaje(string.Format("Ha ocurrido un error inesperado\n{0}",ex), "Error al agregar", MessageBoxIcon.Error);
+            }
+            
+        }
+
+        public void borrarDatos(dynamic dato)
+        {
+            try
+            {
+                DialogResult dialogResult = MessageBox.Show(string.Format("¿Esta seguro de borrar {0}?", dato.Nombre),"Borrando Producto",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                _accesoDatosProducto.EliminarProducto(dato);
+            }
+            catch (Exception ex)
+            {
+                _grafico.Mensaje(string.Format("Ha ocurrido un error inesperado\n{0}", ex), "Error al borrar", MessageBoxIcon.Error);
+            }
+        }
+
+        public void modificarDatos(dynamic dato, dynamic aux_dato)
+        {
+            try
+            {
+                DialogResult dialogResult = MessageBox.Show(string.Format("¿Esta seguro de guardar los cambios en {0}?", aux_dato.Nombre),
+                    "Modificando Producto", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (DialogResult.Yes==dialogResult)
+                    _accesoDatosProducto.ModificarProducto(dato);
+            }
+            catch (Exception ex)
+            {
+                _grafico.Mensaje(string.Format("Ha ocurrido un error inesperado\n{0}", ex), "Error al modificar", MessageBoxIcon.Error);
+            }
+        }
+
+        public void obtenerDatos(DataGridView tabla)
+        {
+            tabla.Columns.Clear();
+            tabla.DataSource = _accesoDatosProducto.ObtenerProductos();
+
+            tabla.AutoResizeColumns();
+        }
     }
 }
