9e1fa61c23945d6e9cbbdfe949fd4dd11c9903fb
Jared Alonso <cacahuate.1895@gmail.com>
2022-10-08 13:10:18 -0500

    Agregando funcionalidad para mostrar datos de herramientas

---
+++ AccesoDatosAgencia/AccesoDatosHerramienta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+using EntidadesAgencia;
+
+namespace AccesoDatosAgencia
+{
+    public class AccesoDatosHerramienta
+    {
+        private Conexion _conexion;
+        public AccesoDatosHerramienta()
+        {
+            _conexion = new Conexion("localhost", "root", "", "agencia", 3306);
+        }
+
+        public List<Herramienta> obtenerHerramientas()
+        {
+            var listHerramienta = new List<Herramienta>();
+            var ds = new DataSet();
+            string consulta = "SELECT * FROM herramienta";
+
+            ds = _conexion.ObtenerDatos(consulta, "herramienta");
+
+            var dt = new DataTable();
+            dt = ds.Tables[0];
+
+            foreach (DataRow row in dt.Rows)
+            {
+                var Herramienta = new Herramienta
+                {
+                    CodigoHerramienta = Convert.ToInt32(row["CodigoHerramienta"].ToString()),
+                    Nombre = row["Nombre"].ToString(),
+                    Medida = Convert.ToDecimal(row["Medida"].ToString()),
+                    Marca = row["Marca"].ToString(),
+                    Descripcion = row["Descripcion"].ToString()
+                };
+                listHerramienta.Add(Herramienta);
+            }
+            return listHerramienta;
+        }
+    }
+}
--- PresentacionAgencia/FrmProductos.Designer.cs
+++ PresentacionAgencia/FrmProductos.Designer.cs
@@ -140,7 +140,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmProductos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Productos";
             this.Load += new System.EventHandler(this.FrmProductos_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
