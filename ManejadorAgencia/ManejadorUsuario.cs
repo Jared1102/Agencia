07f84113239a890c82e5b2642f556a32b0a21828
Jared Alonso <cacahuate.1895@gmail.com>
2022-10-09 12:26:55 -0500

    Agregando manejador para registrar usuario

--- ManejadorAgencia/ManejadorUsuario.cs
+++ ManejadorAgencia/ManejadorUsuario.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AccesoDatosAgencia;
+using EntidadesAgencia;
 
 namespace ManejadorAgencia
 {
-    internal class ManejadorUsuario
+    public class ManejadorUsuario
     {
+        private AccesoDatosUsuarios _accesoDatosUsuarios = new AccesoDatosUsuarios();
+
+        public Usuario ingresarUsuario(string IdUsuario)
+        {
+            return _accesoDatosUsuarios.buscarUsuario(IdUsuario);
+        }
     }
 }
